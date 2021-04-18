d5733da5cb1654c9ee0fd75cb69f2beac82da165
Kartik005 <77089790+Kartik005@users.noreply.github.com>
2021-04-18 21:21:35 +0530

    Add files via upload

---
+++ binary_converter.py
@@ -0,0 +1,26 @@
+your_number = input("Enter binary code: ")
+digits = [int(integer) for integer in str(your_number)]
+digits.reverse()
+n=int(len(digits))
+bin_no = 0
+sum = 0
+i =0
+while(i<= n-1):
+    for num in digits:
+        multiplier= 2**i
+        bin_no = num * multiplier
+        if i== 0:
+            position = "first"
+        elif i == 1:
+            position = "second"
+        elif i == 2:
+            position = "third"
+        else:
+            position = f"{i}th"
+        
+        i+=1
+        
+        print(f"    Value of {position} digits is: {bin_no}")
+        sum = sum + bin_no
+    print(f"\n Value in decimal system is: {sum}")
+    
